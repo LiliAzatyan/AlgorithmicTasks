003bfd59d5eb862ec2d380007089b4d0a0aeb8e1
LiliAzatyan <109226468+LiliAzatyan@users.noreply.github.com>
2023-05-02 00:23:47 +0400

    Update mergeSort.js

--- Task_2/mergeSort.js
+++ Task_2/mergeSort.js
@@ -1,23 +1,30 @@
-function merge(left, right) {
-    let array = []
-    while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            array.push(left.shift())  
-        } else {
-            array.push(right.shift()) 
-        }
-    }
-    return [ ...array, ...left, ...right ]
-}
-
-function mergeSort(array) {
-  const mid = array.length / 2
-  if(array.length < 2){
-    return array 
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr; //base case
   }
-  const left = array.splice(0, mid)
-  return merge(mergeSort(left),mergeSort(array))
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
 }
 
-array = [3, 5, 1, 6, 9, 8, 2];
-console.log(mergeSort(array));
+function merge(left, right) {
+  let result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return result.concat(left, right);
+}
+
+console.log(mergeSort([5, 2, -8, 1, 7, 9, 2, 3]));
